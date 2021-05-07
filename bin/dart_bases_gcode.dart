02edf2b323d2ecb875309205f640f5d3d997cc97
Edwin Garcia <spark.com.co@gmail.com>
2021-05-07 08:32:28 -0500

    Initial commit

---
+++ bin/dart_bases_gcode.dart
@@ -0,0 +1,5 @@
+import 'package:dart_bases_gcode/dart_bases_gcode.dart' as dart_bases_gcode;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_bases_gcode.calculate()}!');
+}
---
+++ test/dart_bases_gcode_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_bases_gcode/dart_bases_gcode.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
